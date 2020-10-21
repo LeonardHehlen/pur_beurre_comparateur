612d47fc9e307e6d4d3676369684c905ac0b3155
LeonardHehlen <leo.hehlen@gmail.com>
2020-10-21 18:01:39 +0200

    Index.html created.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['POST', 'GET'])
 def main():
-    return "Pur beurre Comparateur"
+    return render_template('index.html')
 
 if __name__ == '__main__':
    app.run(debug=True)
